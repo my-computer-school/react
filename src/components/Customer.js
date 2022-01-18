283380daf3be80d2820366828c866621e6460fb3
my-computer-school <gahangwun@gmail.com>
2022-01-18 19:56:48 +0900

    1

---
+++ src/components/Customer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import TableRow from '@material-ui/TableRow';
+import TableCell from '@material-ui/TableCell';
+
+class Customer extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+        
+    }
+}
+
+export default Customer;
