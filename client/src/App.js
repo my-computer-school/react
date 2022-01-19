0ab8b5c3562f0a55c205c40585b2cbae1955b16e
my-computer-school <gahangwun@gmail.com>
2022-01-19 11:13:53 +0900

    1

---
+++ client/src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import Customer from './components/Customer';
+import './App.css';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/any',
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '홍길동2',
+    'birthday': '967222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '홍길동3',
+    'birthday': '961822',
+    'gender': '남자',
+    'job': '대학생'
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            { customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> ); }) }
+          </TableBody>
+        </Table>
+      </div>
+    );
+  }
+}
+
+export default App;
